83f28fc6857695849dd32bd6264bb5d68eec8a86
davidnascimento2 <david.fcn8@gmail.com>
2023-05-25 11:26:26 -0300

    estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+// while (c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+// console.log('Passo 1')
+// console.log('Passo 2')
+// console.log('Passo 3')
+// console.log('Passo 4')
+// console.log('Passo 5')
